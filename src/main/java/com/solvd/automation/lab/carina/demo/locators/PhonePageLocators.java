dd0895e60d6edf0006722134a6ae91b568eaeacd
BabichOlka <64680272+BabichOlka@users.noreply.github.com>
2020-08-05 00:28:21 +0300

    new

--- src/main/java/com/solvd/automation/lab/carina/demo/locators/PhonePageLocators.java
+++ src/main/java/com/solvd/automation/lab/carina/demo/locators/PhonePageLocators.java
@@ -7,5 +7,7 @@ public class PhonePageLocators {
         public static final By MODEL = By.className("specs-phone-name-title");
         public static final By PHOTO = By.xpath("//div[@class='specs-photo-main']//img");
         public static final By SPEC_LIST = By.id("specs-list");
+        public static final By COMMENTS_CONTAINER = By.id("user-comments");
+        public static final By OPTIONS= By.className("article-info-meta");
     }
 }
